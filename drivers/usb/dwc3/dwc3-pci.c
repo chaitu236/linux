653531b47692c651f3eeb2c99b4f10b5ad8c8a5a
Chaitanya Vadrevu <chaitanya.vadrevu@ni.com>
2022-07-22 16:43:08 -0500

    prep for correct fix

--- drivers/usb/dwc3/dwc3-pci.c
+++ drivers/usb/dwc3/dwc3-pci.c
@@ -183,7 +183,7 @@ static int dwc3_pci_quirks(struct dwc3_pci *dwc)
 			 * the GPIOs, add a fallback mapping to the reference
 			 * design GPIOs which all boards seem to use.
 			 */
-			//gpiod_add_lookup_table(&platform_bytcr_gpios);
+			gpiod_add_lookup_table(&platform_bytcr_gpios);
 
 			/*
 			 * These GPIOs will turn on the USB2 PHY. Note that we have to
@@ -307,8 +307,8 @@ static void dwc3_pci_remove(struct pci_dev *pci)
 	struct dwc3_pci		*dwc = pci_get_drvdata(pci);
 	struct pci_dev		*pdev = dwc->pci;
 
-	//if (pdev->device == PCI_DEVICE_ID_INTEL_BYT)
-	//	gpiod_remove_lookup_table(&platform_bytcr_gpios);
+	if (pdev->device == PCI_DEVICE_ID_INTEL_BYT)
+		gpiod_remove_lookup_table(&platform_bytcr_gpios);
 #ifdef CONFIG_PM
 	cancel_work_sync(&dwc->wakeup_work);
 #endif
