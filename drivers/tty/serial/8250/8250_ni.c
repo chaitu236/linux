f9cc3288d22234135adf883922c7f0f2b9ae77ec
Chaitanya Vadrevu <chaitanya.vadrevu@emerson.com>
2025-03-06 20:25:07 -0600

    Revert "fixup! serial: 8250: properly set mctrl for prescaler"
    This reverts commit 925e8e6c1e64f8254ab9d9959b7a290bb0ae894b.

--- drivers/tty/serial/8250/8250_ni.c
+++ drivers/tty/serial/8250/8250_ni.c
@@ -407,12 +407,13 @@ err:
 	return ret;
 }
 
-static void ni16550_remove(struct platform_device *pdev)
+static int ni16550_remove(struct platform_device *pdev)
 {
 	struct ni16550_data *data = platform_get_drvdata(pdev);
 
 	clk_disable_unprepare(data->clk);
 	serial8250_unregister_port(data->line);
+	return 0;
 }
 
 static const struct ni16550_device_info ni16550_default = { };
