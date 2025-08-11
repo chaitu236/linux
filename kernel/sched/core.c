e143c094fd754aa85d604dd7a417d8e17839bfea
Chaitanya Vadrevu <chaitanya.vadrevu@emerson.com>
2025-08-11 13:15:57 -0500

    Revert "panic even on migration task"
    This reverts commit aa2be1be3041c1293f9ebab3c878540e5aaaa40d.
    
    Done testing, so revert

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6785,7 +6785,8 @@ picked:
 		if (cyclictest_task &&
 		    next != cyclictest_task &&
 		    task_cpu(cyclictest_task) == cpu &&
-		    READ_ONCE(cyclictest_task->__state) == TASK_RUNNING) {
+		    READ_ONCE(cyclictest_task->__state) == TASK_RUNNING &&
+		    strncmp(next->comm, "migration", 9) != 0) {
 			panic("cyclictest %d not scheduled cpu %d. next->pid %d",
 			      cyclictest_task->pid, cpu, next->pid);
 		}
